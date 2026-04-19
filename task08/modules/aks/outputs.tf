09b9fa4b0fb2b627d292b945895de5fb342f5b7a
Max Tarasenko <maxtarasenko.0500@gmail.com>
2026-04-19 23:55:57 +0300

    fix: minor fixes

--- task08/modules/aks/outputs.tf
+++ task08/modules/aks/outputs.tf
@@ -1,3 +1,8 @@
 output "kv_identity_client_id" {
   value = azurerm_kubernetes_cluster.aks.key_vault_secrets_provider[0].secret_identity[0].client_id
 }
+
+output "kube_config" {
+  value     = azurerm_kubernetes_cluster.aks.kube_config[0]
+  sensitive = true
+}
